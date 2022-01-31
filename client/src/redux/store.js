412a7744ace950b88a9a283dd06bb20d26b5e4f7
unknown <acemiranda13@att.net>
2022-01-31 14:01:51 -0800

    store - redux dev tool set to development only

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import setAuthToken from '../utils/setAuthToken';
 import rootReducer from './reducers/rootReducer';
